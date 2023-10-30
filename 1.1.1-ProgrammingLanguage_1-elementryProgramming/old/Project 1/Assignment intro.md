74e55c12805d6dde628ee0d47f3e60599c4992e0
CheesyChocolate <dev@behnamlal.xyz>
2023-10-30 19:30:22 +0000

    add session 1.1.1-ProgrammingLanguage_1-elementryProgramming

---
+++ 1.1.1-ProgrammingLanguage_1-elementryProgramming/old/Project 1/Assignment intro.md
@@ -0,0 +1,5 @@
+Solve the question in the attachment
+
+1) Upload java file as a document
+
+2) Paste the source code as text to the Text Field in the screen.
---
+++ 1.1.1-ProgrammingLanguage_1-elementryProgramming/old/Project 1/submissions/digit.displayer.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class QuickStart {
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("enter your five digit number: ");
+        int userrequest=input.nextInt();
+
+        System.out.print("digits are: ");
+
+        int temp = 10000;
+        while (temp > 0) {
+
+            int results = userrequest / temp;
+            System.out.print(results);
+            userrequest = userrequest % temp;
+            temp = temp / 10;
+
+            if (temp > 0) {
+
+                System.out.print("-");
+            }
+        }
+    }
+}
+//Suren(aka Behnam)
